584517d5202581d5981752ef3c8ae852df7ef33c
MaheshUdvag <maheshudvag@gmail.com>
2020-11-15 12:21:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function cricket() {
+    // this function is used to show the cricket background and hide the movie 
+    // background
+    document.getElementById("cricket").style.display = 'block';
+    document.getElementById("movie").style.display = 'none';
+}
+
+function movie() {
+    // this function is used to show the movie background and hide the cricket 
+    // background
+    document.getElementById("movie").style.display = 'block';
+    document.getElementById("cricket").style.display = 'none';
+}
+
